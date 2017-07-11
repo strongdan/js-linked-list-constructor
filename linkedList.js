ec07165d9ee1798998abd3a2fd9664fad2ce694e
Dan Strong <danielstrong@mac.com>
2017-07-11 11:06:29 -0800

    Formatting fixes

--- linkedList.js
+++ linkedList.js
@@ -1,39 +1,39 @@
 function LinkedList() { // linked list constructor
   this.head = null;
   this.tail = null;
-}
+};
 
 function Node(value, next, prev) { // node constructor
   this.value = value;
   this.next = next;
   this.prev = prev;
-}
+};
 
 LinkedList.prototype.addToHead = function(value) {
   var newNode = new Node(value, this.head, null);
-  if (this.head) this.head.prev = newNode; // case that nodes already present
-  else this.tail = newNode; // case that no nodes already present
+  if (this.head) this.head.prev = newNode;
+  else this.tail = newNode;
   this.head = newNode;
-}
+};
+
+LinkedList.prototype.addToTail = function(value) {
+  var newNode = new Node(value, null, this.tail);
+  if (this.tail) this.tail.next = newNode;
+  else this.head = newNode;
+  this.tail = newNode;
+};
 
 LinkedList.prototype.removeHead = function() {
-  if (!this.head) return null // LinkedList is empty
-  var val = this.head.value; 
+  if (!this.head) return null;
+  var val = this.head.value;
   this.head = this.head.next;
   if (this.head) this.head.prev = null;
   else this.tail = null;
   return val;
-}
-
-LinkedList.prototype.addToTail = function(value) {
-  var newNode = new Node(value, null, this.tail);
-  if (this.tail) this.tail.next = newNode; // case that list is not empty
-  else this.head = newNode; // case that list is empty
-  this.tail = newNode;
-}
+};
 
 LinkedList.prototype.removeTail = function() {
-  if (!this.tail) return null; // LinkedList is empty
+  if (!this.tail) return null;
   var val = this.tail.value;
   this.tail = this.tail.prev;
   if (this.tail) this.tail.next = null;
@@ -46,7 +46,7 @@ LinkedList.prototype.search = function(searchValue) {
   while (currentNode) {
     if (currentNode.value === searchValue) return currentNode.value;
     currentNode = currentNode.next;
-  }
+  } 
   return null;
 }
 
@@ -54,12 +54,10 @@ LinkedList.prototype.indexOf = function(value) {
   var indices = [];
   var currentIndex = 0;
   var currentNode = this.head;
-  while (currentNode) {
-    if (currentNode.value === value) {
-      indices.push(currentIndex);
-    }
+  while(currentNode) {
+    if (currentNode.value === value) indexes.push(currentIndex);
     currentNode = currentNode.next;
     currentIndex++;
   }
   return indices;
-}
+};
