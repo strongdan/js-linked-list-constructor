a3da871c4c02e89158041c3d971a65d427f41a58
Dan Strong <danielstrong@mac.com>
2017-07-11 09:50:46 -0800

    Update linkedList.js

--- linkedList.js
+++ linkedList.js
@@ -1,17 +1,42 @@
 function LinkedList() { // linked list constructor
   this.head = null;
   this.tail = null;
-    
 }
 
 function Node(value, next, prev) { // node constructor
   this.value = value;
   this.next = next;
   this.prev = prev;
-  
 }
 
 LinkedList.prototype.addToHead = function(value) {
   var newNode = new Node(value, this.head, null);
+  if (this.head) this.head.prev = newNode; // case that nodes already present
+  else this.tail = newNode; // case that no nodes already present
+  this.head = newNode;
 }
 
+LinkedList.prototype.removeHead = function() {
+  if (!this.head) return null // LinkedList is empty
+  var val = this.head.value; 
+  this.head = this.head.next;
+  if (this.head) this.head.prev = null;
+  else this.tail = null;
+  return val;
+}
+
+LinkedList.prototype.addToTail = function(value) {
+  var newNode = new Node(value, null, this.tail);
+  if (this.tail) this.tail.next = newNode; // case that list is not empty
+  else this.head = newNode; // case that list is empty
+  this.tail = newNode;
+}
+
+LinkedList.prototype.removeTail = function() {
+  if (!this.tail) return null; // LinkedList is empty
+  var val = this.tail.value;
+  this.tail = thsi.tail.prev;
+  if (this.tail) this.tail.next = null;
+  else this.head = null;
+  return val;
+}
